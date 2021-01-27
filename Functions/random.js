40564a923f522747eee254442d9108671de43b03
Heavy-Gray <void@donotreply.com>
2021-01-27 16:59:33 -0700

    Changed to named export;

--- Functions/random.js
+++ Functions/random.js
@@ -4,6 +4,9 @@
  * @return {number} A number in the range of 0 to 100
  * @memberof JS-Skeleton-ES
  */
-export default function random(){
-	return Math.floor(Math.random() * 101 ) 
+
+function random(){
+  return Math.floor(Math.random() * 101 ) 
 }
+
+export default random
